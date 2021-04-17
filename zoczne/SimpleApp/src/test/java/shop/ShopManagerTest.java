31804c9ebe44dd6c1858d88fa256328fa2b41565
kpodlaski <podlaski@uni.lodz.pl>
2021-04-17 11:01:29 +0200

    pom changed

---
+++ zoczne/SimpleApp/src/test/java/shop/ShopManagerTest.java
@@ -0,0 +1,18 @@
+package shop;
+
+
+import database.ShopDatabase;
+import org.junit.jupiter.api.Test;
+
+public class ShopManagerTest {
+
+    @Mock
+    ShopDatabase
+
+
+    @Test
+    public void testAdToCart(){
+
+    }
+
+}
