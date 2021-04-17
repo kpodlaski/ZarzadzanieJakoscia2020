45b249c587cf67fc7f766d0fc722d3a5ada30bc0
kpodlaski <podlaski@uni.lodz.pl>
2021-04-17 11:29:18 +0200

    first test case

--- zoczne/SimpleApp/src/test/java/shop/ShopManagerTest.java
+++ zoczne/SimpleApp/src/test/java/shop/ShopManagerTest.java
@@ -2,17 +2,46 @@ package shop;
 
 
 import database.ShopDatabase;
+import model.Product;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 public class ShopManagerTest {
 
     @Mock
-    ShopDatabase
+    ShopDatabase db;
 
+    ShopManager testObject;
+
+    @BeforeEach
+    public void initTestObject(){
+        System.out.println("Before");
+        db = mock(ShopDatabase.class);
+        testObject = new ShopManager(db);
+    }
 
     @Test
-    public void testAdToCart(){
-
+    public void testAdToCart() throws NoSuchFieldException, IllegalAccessException {
+        Product p = new Product(1,"Doll","doll.jpg",13.35);
+        when(db.getProductById(1)).thenReturn(p);
+        testObject.addProductToCart(1);
+        Field field = testObject.getClass().getDeclaredField("cart");
+        field.setAccessible(true);
+        List<Product> cart = (List<Product>) field.get(testObject);
+        Product p2 = cart.get(0);
+        assertEquals(p,p2);
+        assertEquals(1,cart.size());
+        field.setAccessible(false);
+        verify(db,times(1)).getProductById(1);
+        verify(db,never()).getProductsByName(anyString());
     }
 
 }
